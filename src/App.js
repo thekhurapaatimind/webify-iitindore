83470490db7c17ead65edcf3e13b51f689bc42ee
Abhinav Yadav <actcrick02@gmail.com>
2023-01-15 03:16:22 +0530

    react-bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage';
 import NavbarComponent from './components/NavbarComponent/NavbarComponent';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <NavbarComponent/>
       <LandingPage/>
+      <Routes>
+        <Route exact path='/' element={<LandingPage/>} />
+        <Route exact path='/anime' element={<LandingPage/>} />
+        <Route exact path='/action' element={<LandingPage/>} />
+        <Route exact path='/horror' element={<LandingPage/>} />
+        <Route exact path='/mystery' element={<LandingPage/>} />
+        <Route exact path='/romantic' element={<LandingPage/>} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './NavbarComponent.css'
+import { Link, NavLink } from 'react-router-dom'
+import { Navbar, Nav, Container, Button } from 'react-bootstrap'
 
 function NavbarComponent() {
-  return (
-    <div class="d-none d-md-block navbar">
-    </div>
-  )
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Nav className="m-auto design-nav">
+                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/anime">Anime</Nav.Link>
+                    <Nav.Link as={NavLink} to="/action">Action</Nav.Link>
+                </Nav>
+                <Navbar.Brand as={Link} to="/">MOVIEPEDIA</Navbar.Brand>
+                <Nav className="m-auto">
+                    <Nav.Link as={NavLink} to="/horror">Horror</Nav.Link>
+                    <Nav.Link as={NavLink} to="/mystery">Mystery</Nav.Link>
+                    <Nav.Link as={NavLink} to="/romantic">Romantic</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default NavbarComponent
